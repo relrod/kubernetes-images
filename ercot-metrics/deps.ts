2ca371ebbd03de0124aa129f05f537d03a0f945b
Daniel Lamando <dan@danopia.net>
2021-05-05 11:16:52 +0200

    Update dependencies

--- ercot-metrics/deps.ts
+++ ercot-metrics/deps.ts
@@ -1,14 +1,9 @@
-export { default as DatadogApi } from "https://deno.land/x/datadog_api@v0.1.2/mod.ts";
-export type { MetricSubmission } from "https://deno.land/x/datadog_api@v0.1.2/v1/metrics.ts";
+export { default as DatadogApi } from "https://deno.land/x/datadog_api@v0.1.3/mod.ts";
+export type { MetricSubmission } from "https://deno.land/x/datadog_api@v0.1.3/v1/metrics.ts";
 
-export { fixedInterval } from "https://cloudydeno.github.io/deno-bitesized/logic/fixed-interval@v1.ts";
+export { fixedInterval } from "https://crux.land/4MC9JG#fixed-interval@v1";
 
-export { Sha256 } from "https://deno.land/std@0.87.0/hash/sha256.ts";
+export { Sha256 } from "https://deno.land/std@0.95.0/hash/sha256.ts";
 
-export {
-  runMetricsServer,
-} from "https://raw.githubusercontent.com/cloudydeno/deno-openmetrics_exporter/49ce410657ae5cbd9f647acf1233656933a936aa/mod.ts";
-export {
-  replaceGlobalFetch,
-  fetch,
-} from "https://raw.githubusercontent.com/cloudydeno/deno-openmetrics_exporter/49ce410657ae5cbd9f647acf1233656933a936aa/lib/instrumented/fetch.ts";
+export { runMetricsServer } from "https://deno.land/x/observability@v0.1.0/sinks/openmetrics/server.ts";
+export { replaceGlobalFetch, fetch } from "https://deno.land/x/observability@v0.1.0/sources/fetch.ts";
